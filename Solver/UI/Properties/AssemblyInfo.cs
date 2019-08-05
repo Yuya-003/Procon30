91076ccf0f7c1d89860c08c4abe3cf3f5a665e40
yuya-003 <yu1ohmi@gmail.com>
2019-08-05 17:25:26 +0900

    [修正] UIの作るべきプロジェクトの種類が間違っていたので作り直した

--- Solver/UI/Properties/AssemblyInfo.cs
+++ Solver/UI/Properties/AssemblyInfo.cs
@@ -1,12 +1,10 @@
 ﻿using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
-// アセンブリに関連付けられている情報を変更するには、
-// これらの属性値を変更してください。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
 [assembly: AssemblyTitle("UI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -21,27 +19,10 @@ using System.Windows;
 // その型の ComVisible 属性を true に設定してください。
 [assembly: ComVisible(false)]
 
-//ローカライズ可能なアプリケーションのビルドを開始するには、
-//.csproj ファイルの <UICulture>CultureYouAreCodingWith</UICulture> を
-//<PropertyGroup> 内部で設定します。たとえば、
-//ソース ファイルで英語を使用している場合、<UICulture> を en-US に設定します。次に、
-//下の NeutralResourceLanguage 属性のコメントを解除します。下の行の "en-US" を
-//プロジェクト ファイルの UICulture 設定と一致するよう更新します。
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("0f21de79-86b5-4993-a735-125703dd6c3b")]
 
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //テーマ固有のリソース ディクショナリが置かれている場所
-                                     //(リソースがページ、
-                                     // またはアプリケーション リソース ディクショナリに見つからない場合に使用されます)
-    ResourceDictionaryLocation.SourceAssembly //汎用リソース ディクショナリが置かれている場所
-                                              //(リソースがページ、
-                                              //アプリケーション、またはいずれのテーマ固有のリソース ディクショナリにも見つからない場合に使用されます)
-)]
-
-
-// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
 //
 //      メジャー バージョン
 //      マイナー バージョン
