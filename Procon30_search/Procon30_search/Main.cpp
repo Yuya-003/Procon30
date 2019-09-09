90b51ec52c5d8c22ee4307bc822946d813e77ae7
Shade4827 <seahorses527@gmail.com>
2019-09-09 15:03:59 +0900

    [修正]ファイル入出力部分を修正

--- Procon30_search/Procon30_search/Main.cpp
+++ Procon30_search/Procon30_search/Main.cpp
@@ -10,28 +10,6 @@ void main() {
 	resultData >> str;
 
 	if (str == "none") {
-		//探索した結果を分離
-		std::istringstream stream(str);
-		std::string field;
-		std::vector<std::string> result;
-		while (std::getline(stream, field, '\n')) {
-			result.push_back(field);
-		}
-
-		//結果出力
-		resultData << result[0];
-
-		resultData.clear();
-		if (result.size() >= 2) {
-			for (int i = 1; i < result.size(); i++) {
-				resultData << result[i];
-			}
-		}
-		else {
-			resultData << "none";
-		}
-	}
-	else {
 		BasicSearch bs;
 		FieldInfo fi;
 
@@ -42,16 +20,51 @@ void main() {
 		//FieldInfoに変換
 
 		//探索
-		std::vector<Behaviour> result = bs.Search(fi);
+		std::vector<std::vector<Behaviour>> result = bs.Search(fi);
 
 		//探索結果を出力 → ID:行動:方向
-		fieldData.clear();
 		for (int i = 0; i < fi.allies.size(); i++) {
-			fieldData << fi.allies[i].id << ":" << result[i].action << ":";
-			if (result[i].action != Behaviour::Action::stay) {
-				fieldData << result[i].dir;
+			fieldData << fi.allies[i].id << " : " << result[i][0].action;
+			if (result[i][0].action != Behaviour::Action::stay) {
+				resultData << " : " << result[i][0].dir;
 			}
-			fieldData << std::endl;
+			resultData << std::endl;
+		}
+
+		//2ターン先の行動をターン毎に保存
+		for (int i = 1; i < result[i].size(); i++) {
+			resultData << "[" << i << "]" << std::endl;
+			for (int j = 0; i < fi.allies.size(); j++) {
+				resultData << fi.allies[j].id << ":" << result[j][i].action << ":";
+				if (result[j][i].action != Behaviour::Action::stay) {
+					resultData << result[j][i].dir;
+				}
+				resultData << std::endl;
+			}
+		}
+	}
+	else {
+		//保存された探索結果を分離
+		std::istringstream stream(str);
+		std::string field;
+		std::vector<std::string> result;
+		while (std::getline(stream, field, '\n')) {
+			result.push_back(field);
+		}
+
+		//結果出力
+		std::fstream fieldData("./field.txt", std::ios::out);
+		for (int i = 1; i < result.size(); i++) {
+			if (result[i] == "[2]") { 
+				for (int j = i; j < result.size(); j++) {
+					resultData << result[j] << std::endl;
+				}
+				break; 
+			}
+
+			fieldData << result[i] << std::endl;
+
+			if (i == result.size() - 1) { resultData << "none"; }
 		}
 	}
 }
