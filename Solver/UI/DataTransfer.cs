f0dbc60fe972ab6a538a487ec446bd3189add6c4
yuya-003 <yu1ohmi@gmail.com>
2019-10-03 15:34:06 +0900

    [追加] httpでデータを送受信するクラスを追加

---
+++ Solver/UI/DataTransfer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    class DataTransfer
+    {
+        private static HttpClient client = new HttpClient();
+
+    }
+}
