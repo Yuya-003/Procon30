8d7711593e08da06008b4224bbd2c9db40b219e7
yuya-003 <yu1ohmi@gmail.com>
2019-10-10 08:22:39 +0900

    [変更] ActionJsonをActionsJsonに変更、内部にActionの配列を持つ。

---
+++ Solver/UI/Structure/ActionsJson.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace UI.Structure
+{
+    public class ActionsJson{
+        public class Action
+        {
+            [JsonProperty("agentID")]  //エージェントID
+            public int AgentID { get; set; }
+
+            [JsonProperty("type")]  //行動の種類("move", "remove", "stay")
+            public string Type { get; set; }
+
+            [JsonProperty("dx")]  //行動のx方向の向き
+            public int Dx { get; set; }
+
+            [JsonProperty("dy")]  //行動のy方向の向き
+            public int Dy { get; set; }
+        }
+
+        [JsonProperty("actions")]
+        public Action[] Actions { get; set; }
+
+        public ActionsJson(){}
+
+        public ActionsJson(string str)
+        {
+            ActionsJson actions = JsonConvert.DeserializeObject<ActionsJson>(str);
+
+            Actions = actions.Actions;
+        }
+
+        public static ActionsJson LoadFromJsonFile(string FileName)
+        {
+            var actions = new ActionsJson();
+            using (var sr = new StreamReader(FileName))
+            {
+                string JsonStr = sr.ReadToEnd();
+                actions = JsonConvert.DeserializeObject<ActionsJson>(JsonStr);
+            }
+            
+            return actions;
+        }
+
+        public new string ToString => JsonConvert.SerializeObject(this, Formatting.Indented);
+    }
+
+}
